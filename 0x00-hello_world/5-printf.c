b23d39e98ec9de1dac754af93b58524738907cbf
Augustine Okondor <austinokondor@gmail.com>
2022-09-11 02:50:02 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
 *
 * description - this program prints with proper grammar, but the outcome is a piece of art,\n
 * Return: 0
-
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
