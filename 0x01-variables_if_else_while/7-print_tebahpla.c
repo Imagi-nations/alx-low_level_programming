c17c9f33b0e170aac9f98254262d72a03790b22e
Augustine Okondor <austinokondor@gmail.com>
2022-09-12 05:25:28 -0700

    task 7 betty formatting

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,8 @@
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
- * this program prints the lowercase alphabet in reverse, followed by a new line.
+ * this program prints the lowercase alphabet in reverse,
+ * followed by a new line.
  * Return: 0
  */
 int main(void)
