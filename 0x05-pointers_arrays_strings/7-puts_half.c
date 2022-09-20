bb7d196f6a126d9ad6bade14eedd9354f322f9b3
Augustine Okondor <austinokondor@gmail.com>
2022-09-20 16:49:46 -0700

    tasks 7 arrays...

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,6 +17,13 @@ void puts_half(char *str)
 	putchar(10);
 }
 
+/**
+ * * _strlen - returns the length of a string
+ * * @s: string
+ * *
+ * * Return: the length of the given string
+ * */
+
 int _strlen(char *s)
 {
 	int len = 0;
