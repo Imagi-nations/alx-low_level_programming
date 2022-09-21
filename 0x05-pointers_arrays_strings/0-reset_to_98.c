a87f0075a53767b732d2ac146db726e5fa6dfc47
Augustine Okondor <austinokondor@gmail.com>
2022-09-20 17:05:27 -0700

    last chages to arrays...

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
-* reset_to_98 - function
+*reset_to_98 - function
 *
-* Description: resets int to \98\
-* @*n: parameter
-* Return: void
+*Description: resets int to ninety eight
+*@n: parameter
+*
+*Return: void
 */
 
 void reset_to_98(int *n)
