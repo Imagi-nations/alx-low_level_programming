2bbf04b4a8ba9411932ba1d695cb413fedb1585e
Augustine Okondor <austinokondor@gmail.com>
2022-09-21 16:54:13 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * headerfile prototypes
+ * void prototype()
+ * int prototype()
+ * char prototype()
+ *
+ */
+
+void reverse_array(int *a, int n);
+void print_number(int n);
+void print_buffer(char *b, int size);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+int _putchar(char c);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_string(char *n1, char *n2, char *r, int r_index);
+
+#endif /*MAIN_H*/
