e221aef2dd92fd59779f3d1ec47559da3b3310cb
Augustine Okondor <austinokondor@gmail.com>
2022-09-23 16:17:44 -0700

    0x02-function tasks

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - prints sum of even fibonacci
+*suit elements
+*fibonacci suit numbers
+*Return: return 0
+*/
+int main(void)
+{
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 354224848179261915075)
+{
+n3 = n1 + n2;
+n1 = n2;
+n2 = n3;
+
+if ((n1 % 2) == 0)
+sum += n1;
+
+}
+printf("%ld", ',', ' ', '\0', sum);
+return (0);
+}
